e954e174d1094004ef6de065f8b2cb90598096f3
Angela Weber <angelaweber95@hotmail.com>
2019-11-08 14:08:10 -0800

    make changes to scripts file

--- js/scripts.js
+++ js/scripts.js
@@ -7,31 +7,32 @@ function PizzaOrder(size, toppings,cost){
 
 
 PizzaOrder.prototype.getCost = function() {
-  console.log(this.cost);
-  if(this.size === 8){
-    this.cost += 8;
-  } else if(this.size === 12){
-    this.cost += 12;
-  } else{
-    this.cost += 14;
-  }
-  console.log(this.size);
-  console.log(this.toppings);
-  this.toppings.forEach(function(topping) {
+    this.cost = 5;
+    this.toppings.forEach(function(topping) {
     if(topping === "1"){
       this.cost += 1;
     }
     return this.cost
-  });
-  return this.cost
+    });
+    console.log(this.cost);
+    if(this.size === 8){
+      this.cost += 8;
+    } else if(this.size === 12){
+      this.cost += 12;
+    } else{
+      this.cost += 14;
+    }
+    return this.cost;
+    console.log(this.cost);
 }
+
+
 
 
 // UI LOGIC ----------
 $(document).ready(function(){
-  $('form#pizzaOrder').submit(function(event){
+  $('form#pizzaOrderForm').submit(function(event){
     event.preventDefault();
-    var cost = 5;
     var toppingArray = [];
     var sizeInput = parseInt($("input:radio[name=pizzaSize]:checked").val());
     $("input:checkbox[name='toppings']:checked").each(function(){
@@ -39,10 +40,11 @@ $(document).ready(function(){
      toppingArray.push(topping);
    });
    var pizzaOrder = new PizzaOrder(sizeInput, toppingArray, cost);
+   var cost = pizzaOrder.getCost()
 
 
-    console.log(toppingArray);
     console.log(pizzaOrder);
+    console.log(cost);
     // console.log(pizzaOrder.costCalc());
 
   });
