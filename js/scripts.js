da9a60510a5abbafee053eec834f4cd1025345d7
Angela Weber <angelaweber95@hotmail.com>
2019-11-08 10:18:12 -0800

    add costCalc prototype, and retrieved checkbox data for veggies

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,44 @@
 // BUSINESS LOGIC--------
 function PizzaOrder(size, toppings){
   this.size = size,
-  this.toppings = toppings,
-  this.cost = cost
+  this.toppings = toppings
+  // this.cost = cost
 }
 
+PizzaOrder.prototype.costCalc = function(size, veggies) {
+  this.cost = 5;
+  veggies.forEach(function(veggie){
+    if(veggie === true){
+      this.cost += 1;
+    }
+    console.log("ForEach loop calc",this.cost);
+  });
+  
+  if(size === 8){
+    this.cost += 8;
+  } else if(size === 12){
+    this.cost += 12;
+  } else if(size === 14){
+    this.cost += 14;
+  }
+  return this.cost
+}
 
 // UI LOGIC ----------
 $(document).ready(function(){
   $('form#pizzaOrder').submit(function(event){
-    var sizeInput = parseInt($("input:radio[name='pizzaSize']:checked").val());
-    console.log(sizeInput);
-
-
     event.preventDefault();
+    var pizzaOrder = new PizzaOrder(sizeInput, veggieInputs);
+    var veggieInputs = [];
+    var sizeInput = parseInt($("input:radio[name=pizzaSize]:checked").val());
+    $("input:checkbox[name=veggies]:checked").each(function(){
+      var veggies = $(this).val();
+      veggieInputs.push(veggies);
+
+    });
+
+    console.log(veggieInputs);
+    console.log(pizzaOrder.costCalc(sizeInput, veggieInputs));
+
   });
 });
