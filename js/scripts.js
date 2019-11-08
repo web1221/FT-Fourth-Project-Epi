282cd5ba758c936e6dd862d6b60c51ea26e112ed
Angela Weber <angelaweber95@hotmail.com>
2019-11-08 11:34:04 -0800

    try to make the costCalc prototype function correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,26 @@
 // BUSINESS LOGIC--------
 function PizzaOrder(size, toppings){
   this.size = size,
-  this.toppings = toppings
-  // this.cost = cost
+  this.toppings = toppings,
+  this.cost = 0;
 }
 
-PizzaOrder.prototype.costCalc = function(size, veggies) {
+PizzaOrder.prototype.costCalc = function(size, toppings) {
   this.cost = 5;
-  veggies.forEach(function(veggie){
-    if(veggie === true){
-      this.cost += 1;
-    }
-    console.log("ForEach loop calc",this.cost);
-  });
-  
+  // toppings.forEach(function(topping){
+  //   if(topping === 1){
+  //     return this.cost += 1;
+  //   }else if (topping === 2){
+  //     return this.cost += 2;
+  //   }
+  //   return this.cost;
+  // });
+  for(var i = 0; i > toppings.length; i++){
+    this.cost += toppings[i]
+  }
+  console.log("ForEach loop calc",this.cost);
+
+
   if(size === 8){
     this.cost += 8;
   } else if(size === 12){
@@ -28,17 +35,22 @@ PizzaOrder.prototype.costCalc = function(size, veggies) {
 $(document).ready(function(){
   $('form#pizzaOrder').submit(function(event){
     event.preventDefault();
-    var pizzaOrder = new PizzaOrder(sizeInput, veggieInputs);
-    var veggieInputs = [];
+    var pizzaOrder = new PizzaOrder(sizeInput, toppingInputs);
+    var toppingInputs = [];
     var sizeInput = parseInt($("input:radio[name=pizzaSize]:checked").val());
-    $("input:checkbox[name=veggies]:checked").each(function(){
-      var veggies = $(this).val();
-      veggieInputs.push(veggies);
 
+    $("input:checkbox[name=veggies]:checked").each(function(){
+      var veggies = parseInt($(this).val());
+      toppingInputs.push(veggies);
     });
 
-    console.log(veggieInputs);
-    console.log(pizzaOrder.costCalc(sizeInput, veggieInputs));
+    $("input:checkbox[name=meats]:checked").each(function(){
+      var meats = parseInt($(this).val());
+      toppingInputs.push(meats);
+    });
+    console.log(toppingInputs);
+
+    console.log(pizzaOrder.costCalc(sizeInput, toppingInputs));
 
   });
 });
